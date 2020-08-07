9dde115bd9cd8d9413742b711ed70dd02b5d0726
Farhad16 <fhossain389@gmail.com>
2020-08-07 20:46:20 +0600

    Truthy and Falsy

---
+++ true-false.js
@@ -0,0 +1,14 @@
+// Falsy values 
+// 0 
+// '' empty string
+// null
+// undefined
+// false
+// NaN
+
+const name = undefined
+if (name) {
+    console.log("Condition is True");
+} else {
+    console.log("Condition is False");
+}
